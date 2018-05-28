24618ab4276f8805b6babfbf00dc70c2d532afd9
Angel Hernandez <ahm@Angels-MBP.home>
2018-05-28 16:51:45 -0400

    REDUX LOGIN/READY!

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
@@ -10,6 +15,22 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 
 import './App.css';
+
+// Check for token
+
+if (localStorage.jwtToken) {
+  setAuthToken(localStorage.jwtToken);
+  // decode token gtet user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+
+  const currentTime = Date.now() / 1000;
+  if (decoded.exp < currentTime) {
+    store.dispatch(logoutUser());
+    window.location.href = '/login';
+  }
+}
 
 class App extends Component {
   render() {
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,3 +1,5 @@
+import isEmpty from '../validation/is-empty';
+import { SET_CURRENT_USER } from '../actions/types';
 const initialState = {
   isAuthenticated: false,
   user: {}
@@ -5,8 +7,14 @@ const initialState = {
 
 // This will dispatch the action
 export default function(state = initialState, action) {
-  // console.log('InitialState: ', state, action);
+  // console.log('Set user trigered : ');
   switch (action.type) {
+    case SET_CURRENT_USER:
+      return {
+        ...state,
+        isAuthenticated: !isEmpty(action.payload),
+        user: action.payload
+      };
     default:
       return state;
   }
